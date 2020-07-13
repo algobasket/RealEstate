bdfcaa418520764346bf5852cbcc9730ee10582a
algobasket <algobasket@gmail.com>
2020-07-13 08:13:30 +0530

    frontend pages

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('buy/', 'Home::buy');
+$routes->get('sell/', 'Home::sell');
+$routes->get('rent/', 'Home::rent');    
 
 /**
  * --------------------------------------------------------------------
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,10 +4,56 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		//$data['page'] = "landing";
-		return view('common/content');     
+		$data['title'] = "Welcome to PropertyRaja";
+		return view('landing',$data);      
 	}
 
-	
+	public function buy()  
+	{
+       $data['title'] = "Buy Properties";
+	   return view('landing',$data); 
+	}
 
+	public function sell()
+	{
+		$data['sell'] = "Sell Properties";
+	    return view('frontend/sell',$data);
+	}
+
+	public function rent()
+	{
+		$data['rent'] = "Rent Properties";
+	    return view('frontend/rent',$data);
+	}
+
+	public function browse() 
+	{
+		$data['browse'] = "Browse Properties";
+	    return view('frontend/browse',$data);
+	}
+    
+    public function contact()
+    {
+       $data['contact'] = "Contact Us";
+	   return view('frontend/contact',$data);
+    }
+
+	public function about()
+    {
+       $data['rent'] = "About Us";
+	   return view('frontend/about',$data);
+    } 
+    
+	public function policy()
+    {
+       $data['policy'] = "Our Policy";
+	   return view('frontend/policy',$data);
+    }
+    
+	public function support()
+    {
+       $data['support'] = "Customer Support";
+	   return view('frontend/support',$data);
+    } 
+    
 }
